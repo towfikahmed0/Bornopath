85371a3e29b6a771ccdb342030cd8955ec31e865
Towfik Ahmed <107678190+towfikahmed0@users.noreply.github.com>
2025-07-18 21:16:22 +0600

    api key removed from sys.js due to security concerns

--- js/sys.js
+++ js/sys.js
@@ -18,7 +18,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB12GMrNdELvkdSKxF8Ij2IGKRqUh63WTc",
+    apiKey: "",
     authDomain: "wordvo-bb47d.firebaseapp.com",
     projectId: "wordvo-bb47d",
     storageBucket: "wordvo-bb47d.firebasestorage.app",
